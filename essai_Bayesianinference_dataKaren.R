8602eaa363d9bb65e7029498c81e8ee141e5e5d7
Sophie Donnet <sophie.donnet@agroparistech.fr>
2022-11-10 18:09:51 +0100

    dadareal

--- essai_Bayesianinference_dataKaren.R
+++ essai_Bayesianinference_dataKaren.R
@@ -18,31 +18,32 @@ T_Exp_V <- read.table("DataKaren/TVExp.txt", quote="\"", comment.char="")$V1
 
 
 k <- 16
-kprime <- 30
-
-
-
-
-
-par(mfrow=c(2,2))
-hist(T_V_C,freq = FALSE,main="Natural Death. GREEN")  
-hist(T_R_C,freq = FALSE,main="Natural Death. RED")  
-hist(T_Exp_V,freq=FALSE,nclass =min(n_V/10,100), main=paste("Experi Data. GREEN",sep=' '))  
-hist(T_Exp_R,freq=FALSE,nclass =min(n_V/10,100), main=paste("Experi Data. RED",sep=' '))  
-
-
+kprime <- 32
 
 
 mydata  <- list()
-mydata$T_Contr_R <- T_R_C*2
-mydata$T_Contr_V <- T_V_C
+mydata$T_Contr_R <- T_R_C*2.8
+mydata$T_Contr_V <- T_V_C*1.4
 mydata$k <- k
 mydata$kprime <- kprime
-mydata$Tmax <- 140
-mydata$T_Exp_R <- T_Exp_R*2
-mydata$T_Exp_V <- T_Exp_V
+mydata$Tmax <-max(mydata$T_Contr_V)
+mydata$T_Exp_R <- T_Exp_R*2.8
+mydata$T_Exp_V <- T_Exp_V*1.4
 mydata$n_Exp_R <- length(mydata$T_Exp_R)
 mydata$n_Exp_V <- length(mydata$T_Exp_V)
+
+Tmax <- max(mydata$T_Contr_V)
+mydata$T_Contr_R[mydata$T_Contr_R>Tmax] = Tmax
+mydata$T_Exp_R[mydata$T_Exp_R>Tmax] = Tmax
+
+
+par(mfrow=c(2,2))
+hist(mydata$T_Contr_V,nclass=50,freq = FALSE,main="Natural Death. GREEN")  
+hist(mydata$T_Contr_R,nclass=50,freq = FALSE,main="Natural Death. RED")  
+hist(mydata$T_Exp_V,nclass=50,freq=FALSE, main=paste("Experi Data. GREEN",sep=' '))  
+hist(mydata$T_Exp_R,nclass=50,freq=FALSE, main=paste("Experi Data. RED",sep=' '))  
+
+
 
 ########################################################################
 ################################# ESTIM ND  
@@ -74,13 +75,13 @@ load('essais_datasimu_param.Rdata')
 par(mfrow=c(ceiling(length(paramsChains$paramsToSample)/2),2))
 for(m in paramsChains$paramsToSample){
   plot(mySample$myLogPostSample[,m],type='l',xlab='',main=names(mySample$myPostSample)[m],ylab=''); 
-  abline(h=log_param_true[m],col='green',lwd=2)
+#  abline(h=log_param_true[m],col='green',lwd=2)
 }
 
 par(mfrow=c(ceiling(length(paramsChains$paramsToSample)/2),2))
 for(m in paramsChains$paramsToSample){
   plot(density(mySample$myLogPostSample[,m]),main=names(mySample$myLogPostSample)[m])
-  abline(v= log_param_true[m],col='green',lwd=2)
+#  abline(v= log_param_true[m],col='green',lwd=2)
   curve(dnorm(x,hyperparams$mean[m],hyperparams$sd[m]),add=TRUE,col='orange',lwd=2)
 }
 
@@ -93,13 +94,12 @@ param_estim <- log_param_estim
 param_estim[1:5] <- exp(log_param_estim[1:5])
 param_estim[6] <- 1/(1+exp(-log_param_estim[6]))
 
-P <- computationToPlotCurves(abs, param_true,mydata$k,mydata$kprime)
-P$comp <- "True"
+#P <- computationToPlotCurves(abs, param_true,mydata$k,mydata$kprime)
+#P$comp <- "True"
 Pestim <- computationToPlotCurves(abs, param_estim,mydata$k,mydata$kprime)
 Pestim$comp <- "Estim"
-allP <- rbind(P,Pestim)
+allP <- Pestim
 
-levels(P$Curves)
 Ptemp <- allP %>% filter(Curves %in% c('2. Sum Arrival + reading', '3. min(ND,Reading)'))
 
 g <- ggplot(data = Ptemp, aes(x=time,y=density,colour = Curves))+ geom_line(aes(colour=Curves, linetype=comp))#+  geom_point(aes(shape=comp)) 
@@ -109,15 +109,14 @@ g + facet_wrap(~Color_Part)
 
 
 par(mfrow=c(1,2))
-begin_title <- ifelse(param_true[6] > 0,'With','Without')
-hist(mydata$T_Exp_V,freq=FALSE,nclass =min(mydata$n_Exp_V/10,100), main=paste(begin_title,"Quick Death. Green",sep=' '))  
+#begin_title <- ifelse(param_true[6] > 0,'With','Without')
+hist(mydata$T_Exp_V,freq=FALSE,nclass =min(mydata$n_Exp_V/10,100), main=paste("Quick Death. Green",sep=' '))  
 lines(density(mydata$T_Exp_V))
-curve(dOurModel(x,param_true,mydata$k,mydata$kprime,color='green',Tmax=mydata$Tmax),add=TRUE,col='green',lwd=2,lty = 1)
+#curve(dOurModel(x,param_true,mydata$k,mydata$kprime,color='green',Tmax=mydata$Tmax),add=TRUE,col='green',lwd=2,lty = 1)
 curve(dOurModel(x,param_estim,mydata$k,mydata$kprime,color='green',Tmax = mydata$Tmax),add=TRUE,col='orange',lwd=2,lty = 2)
 
-hist(mydata$T_Exp_R,freq=FALSE,nclass= min(mydata$n_Exp_R/10,100), main=paste(begin_title,"Quick Death. Red",sep=' '))  
+hist(mydata$T_Exp_R,freq=FALSE,nclass= min(mydata$n_Exp_R/10,100), main=paste("Quick Death. Red",sep=' '))  
 lines(density(mydata$T_Exp_R))
-curve(dOurModel(x,param_true,mydata$k,mydata$kprime,color='red',Tmax = mydata$Tmax),add=TRUE,col='red',lwd=2,lty = 1)
 curve(dOurModel(x,param_estim,mydata$k,mydata$kprime,color='red',Tmax = mydata$Tmax),add=TRUE,col='orange',lwd=2,lty = 2)
 
 
